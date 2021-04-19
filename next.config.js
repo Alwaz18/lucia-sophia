9402414eebbf2db98f9382b750c59375d5f3deea
Alwaz18 <52915358+Alwaz18@users.noreply.github.com>
2021-04-19 11:38:22 +0500

    final

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-
+  env: {
+    STRAPI_GRAPHQL_URL: 'https://rocky-ocean-85600.herokuapp.com/graphql',
+    STRAPI_URL: 'https://rocky-ocean-85600.herokuapp.com',
+    SERVICE_ID: 'service_x1vtaeg',
+    TEMPLATE_ID: 'template_a3ki6qb',
+    USER_ID: 'user_fcLsvvjdcR8yEn0OQzA26'
+  },
   future: {
     webpack5: true,
   },
 
   images: {
-    domains: ["res.cloudinary.com", "cloudinary.com","images.unsplash.com","unsplash.com","localhost"],
-    loader:'default',
-   
+    domains: ["res.cloudinary.com", "cloudinary.com", "images.unsplash.com", "unsplash.com", "localhost"],
+    loader: 'default',
+
   },
   async headers() {
     return [
